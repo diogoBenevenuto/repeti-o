534813af3d91de6d875a20a3aed976c0f6db843b
diogoBenevenuto <63623546+diogoBenevenuto@users.noreply.github.com>
2020-05-26 20:26:52 -0300

    ex1

---
+++ ex1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner leitor = new Scanner (System.in);
+		int x = 0;
+		
+		
+		while (x < 10) {
+			
+			System.out.println("digite um numero: ");
+			x =leitor.nextInt();
+		
+		}
+		
+		System.out.println("saiu da estrutura ");
+	}
+
+}
