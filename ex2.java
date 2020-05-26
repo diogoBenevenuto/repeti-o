f9bab49a34a8b04c4e20cf70b2995cf81dbb187a
diogoBenevenuto <63623546+diogoBenevenuto@users.noreply.github.com>
2020-05-26 20:27:15 -0300

    ex2

---
+++ ex2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ex2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		Scanner leitor = new Scanner (System.in);
+		int num = 1;
+		int numero = 0;
+		
+		System.out.println("Digite um numero:");
+		numero = leitor.nextInt();
+		
+		while (num < numero ) {
+			
+			
+			System.out.println(num);
+			num++;
+		}
+		
+		
+		
+		
+	}
+
+}
